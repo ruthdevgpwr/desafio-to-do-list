a253537f3574b02a239251aaf16e6c5326767e37
Ruth Dantas <ruthdevgp@gmail.com>
2023-02-06 19:22:11 -0300

    adicionando o componente header e criando o input para adicionar tarefas

--- desafio-to-do-list/src/App.tsx
+++ desafio-to-do-list/src/App.tsx
@@ -1,10 +1,24 @@
+import {Header} from './components/Header';
+import styles from './App.module.css';
+import {PlusCircle} from 'phosphor-react';
 import './global.css';
 
-function App() {
+export function App() {
 
   return (
-    <p>Hello World</p>
+    <>
+      <Header />
+
+      <form className={styles.formTask}>
+        <input className={styles.inputTask} type="text" placeholder='Adicione uma nova tarefa' />
+
+        <button className={styles.buttonTask} type='submit'>
+            Criar
+          <span className={styles.iconButton}>
+            <PlusCircle />
+          </span>
+        </button>
+      </form>
+    </>
   )
 }
-
-export default App
