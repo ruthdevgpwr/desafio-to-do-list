20128aa8793497432e31deb84bf9da2109b81227
Ruth Dantas <ruthdevgp@gmail.com>
2023-02-06 19:24:34 -0300

    adicionando a logo, criando a pasta componentes, estilizando o componente header e estilizando botao do formulário

---
+++ desafio-to-do-list/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+import logo from '../assets/logo.svg';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="logo" />
+    </header>
+  )
+}
